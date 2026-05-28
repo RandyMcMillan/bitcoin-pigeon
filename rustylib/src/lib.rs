6e38048e02c70e1c79188c7792062a93ad5c0f4b
randymcmillan <randymcmillan@protonmail.com>
2026-05-28 15:22:17 -0400

    rustylib

--- rustylib/src/lib.rs
+++ rustylib/src/lib.rs
@@ -1,11 +1,9 @@
 uniffi::setup_scaffolding!();
- 
-#[uniffi::export]
-fn rust_hello() -> String {
-    "Hello from Rust!".to_string()
-}
 
-#[uniffi::export]
-pub fn rust_add(a: u32, b: u32) -> u32 {
-    a + b
+#[uniffi::export(async_runtime = "tokio")]
+pub async fn blast_transaction_hex(tx_hex: String) -> Result<u32, String> {
+    tx_pigeon::blast_transaction_hex(&tx_hex)
+        .await
+        .map(|count| count as u32)
+        .map_err(|error| error.to_string())
 }
